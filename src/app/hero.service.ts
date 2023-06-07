de48ee6fe3938d711fae109793925370f447bfe3
Paolo Villaluna <pmf.villaluna@gmail.com>
2023-06-07 22:26:13 +0800

    feature/add routing

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,18 +1,35 @@
 import { Injectable } from '@angular/core';
-import { Hero } from './Hero';
-import { HEROES } from './mock-heroes';
+
 import { Observable, of } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+import { Hero } from './Hero';
+import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+@Injectable({ providedIn: 'root' })
 export class HeroService {
+  private heroesUrl = 'api/heroes';  // URL to web api
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
 
+  /** GET heroes from the server */
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl)
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    // For now, assume that a hero with the specified `id` always exists.
+    // Error handling will be added in the next step of the tutorial.
+    const hero = HEROES.find(h => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
   }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,33 +2,25 @@ import { Component } from '@angular/core';
 import { Hero } from '../Hero';
 import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
-
+import { MessageService } from '../message.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
 
 })
+
 export class HeroesComponent {
-  heroes: Hero [] = [];
-  selectedHero?: Hero;
+  heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService) {}
+  constructor(private heroService: HeroService) { }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
-  getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+  ngOnInit(): void {
+    this.getHeroes();
   }
 
   getHeroes(): void {
     this.heroService.getHeroes()
-    .subscribe(heroes=> this.heroes = heroes)
-  }
-
-  ngOnInit(): void {
-    this.getHeroes();
+    .subscribe(heroes => this.heroes = heroes);
   }
 }
