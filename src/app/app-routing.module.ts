9200b060686eb0f778ce5b5bdf965c161b045a90
Paolo Villaluna <pmf.villaluna@gmail.com>
2023-07-11 18:45:23 +0800

    fix/delete unused files
    Delete files for no longer for use

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { ExampleDashboardComponent } from './components/example/example-dashboard/example-dashboard.component';
 import { PersonDashboardComponent } from './components/person/person-dashboard/person-dashboard.component';
-import { ExampleAddFormComponent } from './components/example/example-add-form/example-add-form.component';
 const routes: Routes = [
   {path: 'person', component: PersonDashboardComponent},
   { path: '', redirectTo: '/person', pathMatch: 'full' },
-  {path:'sample', component: ExampleDashboardComponent},
-  {path:'sample-add', component: ExampleAddFormComponent},
-  {path:'sample-add/:id', component: ExampleAddFormComponent}
 
 ]
 
